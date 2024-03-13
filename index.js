f4de8ac3b81ae3f71d9aad59feb3b541326e1512
jonathanbanerjee <jonathan@youd.net>
2024-03-13 00:33:17 +0000

    adding alerts as buttons

---
+++ index.js
@@ -0,0 +1,10 @@
+// let addButton = document.getElementById("add");
+// let removeButton = document.getElementById("remove");
+
+document.getElementById("add").addEventListener("click", function () {
+  alert("Ouch! Stop poking me!");
+});
+
+document.getElementById("remove").addEventListener("click", function () {
+  alert("Stop poking me too!");
+});
