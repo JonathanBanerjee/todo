cbac0de4a2ea7b4937c61a94858c7e4f6c404254
jonathanbanerjee <jonathan@youd.net>
2024-03-15 15:57:32 +0000

    adding latest

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-// let addButton = document.getElementById("add");
-// let removeButton = document.getElementById("remove");
+let input = document.getElementById("userInput");
+let firstItem = document.getElementById("list-0");
+let list = document.getElementById("myList");
 
 document.getElementById("add").addEventListener("click", function () {
-  alert("Ouch! Stop poking me!");
+  // console.log(input, "hello world");
+  // firstItem.innerHTML = input.value;
+  const newLi = document.createElement("li");
+
+  // and give it some content
+  const newItem = document.createTextNode(input.value);
+
+  // add the text node to the newly created div
+  newLi.appendChild(newItem);
+
+  list.appendChild(newLi);
+  Event.preventDefault();
 });
 
-document.getElementById("remove").addEventListener("click", function () {
-  alert("Stop poking me too!");
-});
+// document.getElementById("remove").addEventListener("click", function () {
+//   alert("Stop poking me too!");
+// });
