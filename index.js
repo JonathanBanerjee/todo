6c6d5ebd3a4625c4a45f528f8367859216ec8e15
jonathanbanerjee <jonathan@youd.net>
2024-03-17 15:45:42 +0000

    remove items

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 let input = document.getElementById("userInput");
 let firstItem = document.getElementById("list-0");
 let list = document.getElementById("myList");
+let newLi;
 
-document.getElementById("add").addEventListener("click", function () {
+document.getElementById("add").addEventListener("click", function (e) {
   // console.log(input, "hello world");
   // firstItem.innerHTML = input.value;
-  const newLi = document.createElement("li");
+  newLi = document.createElement("li");
 
   // and give it some content
   const newItem = document.createTextNode(input.value);
@@ -14,9 +15,13 @@ document.getElementById("add").addEventListener("click", function () {
   newLi.appendChild(newItem);
 
   list.appendChild(newLi);
-  Event.preventDefault();
+  e.preventDefault();
 });
 
-// document.getElementById("remove").addEventListener("click", function () {
-//   alert("Stop poking me too!");
-// });
+document.getElementById("remove").addEventListener("click", function (e) {
+  newLi.remove();
+});
+
+document.getElementById("edit").addEventListener("click", function (e) {
+  console.log(newLi);
+});
