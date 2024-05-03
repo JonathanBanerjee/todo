d1afab8a231b403c7a5613531527bb31fff238ba
jonathanbanerjee <jonathan@youd.net>
2024-05-03 23:06:56 +0100

    adding background

--- index.js
+++ index.js
@@ -35,7 +35,8 @@ document.getElementById("add").addEventListener("click", function (e) {
   editButton.textContent = "Edit 📝";
   editButton.addEventListener("click", function (e) {
     newItem.contentEditable = true;
-    newItem.style.backgroundColor = "blue";
+    newItem.style.backgroundColor = "#ccc5b9";
+    newItem.style.color = "white";
     e.stopPropagation();
   });
 
@@ -45,15 +46,22 @@ document.getElementById("add").addEventListener("click", function (e) {
   saveButton.addEventListener("click", function (e) {
     newItem.contentEditable = false;
     newItem.style.backgroundColor = "#eaddcf";
+    newItem.style.color = "black";
     e.stopPropagation();
   });
 
-  newLi.appendChild(saveButton);
-  newLi.appendChild(editButton);
-  newLi.appendChild(removeButton);
-  newLi.appendChild(completed);
+  const buttonContainer = document.createElement("div");
+  buttonContainer.classList.add("button-container");
 
-  newLi.appendChild(newItem);
+  buttonContainer.appendChild(saveButton);
+  buttonContainer.appendChild(editButton);
+  buttonContainer.appendChild(removeButton);
+  buttonContainer.appendChild(completed);
+  buttonContainer.appendChild(newItem);
+
+  newLi.appendChild(buttonContainer);
+
+  // newLi.appendChild(newItem);
 
   list.appendChild(newLi);
   e.preventDefault();
