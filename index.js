ce4ca78e1ddd041f40a71f4fe72d8ccd351a3035
jonathanbanerjee <jonathan@youd.net>
2024-07-22 14:56:30 +0100

    adding local storage

--- index.js
+++ index.js
@@ -1,89 +1,97 @@
-// let tasksArray = localStorage.getItem("tasks")
-//   ? JSON.parse(localStorage.getItem("tasks"))
-//   : [];
+let tasksArray = localStorage.getItem("tasks")
+  ? JSON.parse(localStorage.getItem("tasks"))
+  : [];
 
+console.log(tasksArray);
 let input = document.getElementById("userInput");
 input.focus();
 let list = document.getElementById("myList");
 
+renderList();
 // Add a task
 document.getElementById("add").addEventListener("click", function (e) {
   e.preventDefault();
   // Reading what is in local storage
-  const tasks = localStorage.getItem("tasks");
+  const tasks = JSON.parse(localStorage.getItem("tasks"));
   console.log(tasks);
   if (!tasks) {
     let firstItem = [];
-    const newArray = firstItem.push(input.value);
-    localStorage.setItem("tasks", newArray);
-    console.log(input.value);
+    firstItem.push(input.value);
+    console.log(firstItem);
+    localStorage.setItem("tasks", JSON.stringify(firstItem));
   } else {
-    [...tasks].push(input.value);
-    localStorage.setItem("tasks", tasks);
+    tasks.push(input.value);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
   }
-
-  console.log("Code was running");
-  // On click, create a new list item
-  let newLi = document.createElement("li");
-  newLi.classList.add("listitems");
-
-  const newItem = document.createElement("p");
-  newItem.innerText = input.value;
-  newItem.classList.add("task");
-
+  tasksArray.push(input.value);
+  renderList();
   input.value = "";
   input.focus();
-
-  //Create a checkbox
-  const completed = document.createElement("input");
-  completed.type = "checkbox";
-  newItem.classList.add("completed");
-
-  completed.addEventListener("change", function () {
-    this.checked
-      ? (newItem.style.textDecoration = "line-through")
-      : (newItem.style.textDecoration = "none");
-  });
-
-  //Adding remove button to each item.
-  const removeButton = document.createElement("button");
-  removeButton.textContent = "Remove 🗑️";
-  removeButton.addEventListener("click", function (e) {
-    newLi.remove();
-    localStorage.removeItem();
-  });
-
-  // Adding edit button to each item.
-  const editButton = document.createElement("button");
-  editButton.textContent = "Edit 📝";
-  editButton.addEventListener("click", function (e) {
-    newItem.contentEditable = true;
-    newItem.style.backgroundColor = "gainsboro";
-    newItem.style.color = "black";
-    newItem.focus();
-    e.stopPropagation();
-  });
-
-  // Adding a save button to each item.
-  const saveButton = document.createElement("button");
-  saveButton.textContent = "Save 🛟";
-  saveButton.addEventListener("click", function (e) {
-    newItem.contentEditable = false;
-    newItem.style.backgroundColor = "";
-    e.stopPropagation();
-  });
-
-  const buttonContainer = document.createElement("div");
-  buttonContainer.classList.add("button-container");
-
-  buttonContainer.appendChild(newItem);
-  buttonContainer.appendChild(saveButton);
-  buttonContainer.appendChild(editButton);
-  buttonContainer.appendChild(removeButton);
-  buttonContainer.appendChild(completed);
-
-  newLi.appendChild(buttonContainer);
-
-  list.appendChild(newLi);
-  e.preventDefault();
 });
+
+// On click, create a new list item
+function renderList() {
+  const outputArray = tasksArray.map((e) => `<li>${e}</li>`);
+  outputArray.join(" ");
+  list.innerHTML = outputArray;
+
+  // let newLi = document.createElement("li");
+  // newLi.classList.add("listitems");
+
+  // const newItem = document.createElement("p");
+  // newItem.innerText = input;
+  // newItem.classList.add("task");
+
+  // //Create a checkbox
+  // const completed = document.createElement("input");
+  // completed.type = "checkbox";
+  // newItem.classList.add("completed");
+
+  // completed.addEventListener("change", function () {
+  //   this.checked
+  //     ? (newItem.style.textDecoration = "line-through")
+  //     : (newItem.style.textDecoration = "none");
+  // });
+
+  // //Adding remove button to each item.
+  // const removeButton = document.createElement("button");
+  // removeButton.textContent = "Remove 🗑️";
+  // removeButton.addEventListener("click", function (e) {
+  //   newLi.remove();
+  //   localStorage.removeItem();
+  // });
+
+  // // Adding edit button to each item.
+  // const editButton = document.createElement("button");
+  // editButton.textContent = "Edit 📝";
+  // editButton.addEventListener("click", function (e) {
+  //   newItem.contentEditable = true;
+  //   newItem.style.backgroundColor = "gainsboro";
+  //   newItem.style.color = "black";
+  //   newItem.focus();
+  //   e.stopPropagation();
+  // });
+
+  // // Adding a save button to each item.
+  // const saveButton = document.createElement("button");
+  // saveButton.textContent = "Save 🛟";
+  // saveButton.addEventListener("click", function (e) {
+  //   newItem.contentEditable = false;
+  //   newItem.style.backgroundColor = "";
+  //   e.stopPropagation();
+  // });
+
+  // const buttonContainer = document.createElement("div");
+  // buttonContainer.classList.add("button-container");
+
+  // buttonContainer.appendChild(newItem);
+  // buttonContainer.appendChild(saveButton);
+  // buttonContainer.appendChild(editButton);
+  // buttonContainer.appendChild(removeButton);
+  // buttonContainer.appendChild(completed);
+
+  // newLi.appendChild(buttonContainer);
+
+  // list.appendChild(newLi);
+  // e.preventDefault();
+}
